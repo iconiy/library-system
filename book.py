eb7be8218f519b23651bd13a8b8368909c7dde29
iconiy <greg91moore@gmail.com>
2025-01-30 18:19:15 -0800

    Created book class and setup checkout

---
+++ book.py
@@ -0,0 +1,19 @@
+
+class Book:
+
+    def __init__(self, title, author, year, genre, isbn, rental_length):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.genre = genre
+        self.isbn = isbn
+        self.rental_length = rental_length
+
+    def check_out(self):
+        rental_length = 14
+        print(f'You are about to check out {self.title} by {self.author} for {rental_length} days.')
+        choice = input('Press Enter to check out...')
+        if choice == '':
+            print(f'You have successfully checked out {self.title} by {self.author}.')
+
+    def get_info(self):
